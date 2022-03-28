baff2de5ba2540af2fb15a49d10a5f7721d5dcd0
donmiguel9 <michaelgubler@gmx.net>
2022-03-28 19:02:17 +0200

    reversed

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ import Footer from "../components/Layout/Footer";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <div className="font-kanit bg-[#BDC5D8]">
+      <div className="font-kanit">
         <Navbar />
         <Component {...pageProps} />
         <Footer />
