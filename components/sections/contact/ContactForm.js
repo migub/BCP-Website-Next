bc1244ca133677762c9b93617eee6f0c3572bae7
donmiguel9 <michaelgubler@gmx.net>
2022-05-30 15:12:34 +0200

    added contact API and sucess animation

--- components/sections/contact/ContactForm.js
+++ components/sections/contact/ContactForm.js
@@ -1,7 +1,26 @@
 import { useState } from "react";
+import Lottie from "react-lottie";
+import * as animationData from "../../../public/lottie/success.json";
 
 const ContactForm = (props) => {
-  const [ticked, setTicked] = useState(false);
+  const defaultOptions = {
+    loop: false,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+  const [sendText, setSendText] = useState("Send");
+  const [buttonActive, setButtonActive] = useState(true);
+  const [sendButton, setSendButton] = useState(
+    "py-2 px-4  bg-[#383f56] hover:bg-gray-600 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+  );
 
   return (
     <>
@@ -12,97 +31,92 @@ const ContactForm = (props) => {
         className="flex w-full max-w-xl m-auto space-x-3"
       >
         <div className="w-full max-w-3xl px-5 py-10 m-auto bg-[#EDEFF5] rounded-lg shadow">
-          {/* <div className="mb-6 text-3xl font-light text-center text-gray-800 dark:text-white">
-            Contact us !
-          </div> */}
-          <div className="grid max-w-xl grid-cols-2 gap-4 m-auto">
-            <div className="col-span-2 lg:col-span-1">
-              <div className="relative ">
-                <input
-                  type="text"
-                  id="contact-form-name"
-                  className="flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border border-transparent border-gray-300 rounded-lg shadow-sm appearance-none focus:outline-none focus:ring-2 focus:ring-[#383f56] focus:border-transparent"
-                  placeholder={props.placeholderOne}
-                />
+          {submitted ? (
+            <>
+              <Lottie options={defaultOptions} height={200} width={200} />
+              <div className="mb-10 text-center">
+                Message sent! We will get back to you as soon as possible.
               </div>
-            </div>
-            <div className="col-span-2 lg:col-span-1">
-              <div className="relative ">
-                <input
-                  type="text"
-                  id="contact-form-email"
-                  className="flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border border-transparent border-gray-300 rounded-lg shadow-sm appearance-none focus:outline-none focus:ring-2 focus:ring-[#383f56] focus:border-transparent"
-                  placeholder={props.placeholderTwo}
-                />
-              </div>
-            </div>
-            <div className="col-span-2">
-              <label className="text-gray-700" htmlFor="name">
-                <textarea
-                  className="flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border border-gray-300 rounded-lg appearance-none focus:outline-none focus:ring-2 focus:ring-[#383f56] focus:border-transparent"
-                  id="comment"
-                  placeholder={props.placeholderThree}
-                  name="comment"
-                  rows="5"
-                  cols="40"
-                ></textarea>
-              </label>
-            </div>
-
-            {/*  */}
-            {/* <div className="bg-cyan-500 ">
-              <div>
-                <div className="flex items-center justify-center mt-6">
-                  <label className="flex items-center mb-3 space-x-3">
-                    <input
-                      onClick={() => setTicked(!ticked)}
-                      type="checkbox"
-                      name="checked-demo"
-                      className="w-6 h-6 ml-10 bg-white border border-white rounded-md form-tick bg-check checked:bg-blue-500 checked:border-transparent focus:outline-none"
-                    />
-                    <span className="font-normal text-gray-700 whitespace-nowrap dark:text-white">
-                      Do you want to invest?
-                    </span>
-                  </label>
+            </>
+          ) : (
+            <div className="grid max-w-xl grid-cols-2 gap-4 m-auto">
+              <div className="col-span-2 lg:col-span-1">
+                <div className="relative ">
+                  <input
+                    onChange={(e) => {
+                      setName(e.target.value);
+                    }}
+                    type="text"
+                    id="contact-form-name"
+                    value={name}
+                    className="flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border border-transparent border-gray-300 rounded-lg shadow-sm appearance-none focus:outline-none focus:ring-2 focus:ring-[#383f56] focus:border-transparent"
+                    placeholder={props.placeholderOne}
+                  />
                 </div>
               </div>
+              <div className="col-span-2 lg:col-span-1">
+                <div className="relative ">
+                  <input
+                    onChange={(e) => setEmail(e.target.value)}
+                    type="text"
+                    id="contact-form-email"
+                    value={email}
+                    className="flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border border-transparent border-gray-300 rounded-lg shadow-sm appearance-none focus:outline-none focus:ring-2 focus:ring-[#383f56] focus:border-transparent"
+                    placeholder={props.placeholderTwo}
+                  />
+                </div>
+              </div>
+              <div className="col-span-2">
+                <label className="text-gray-700" htmlFor="name">
+                  <textarea
+                    onChange={(e) => setMessage(e.target.value)}
+                    className="flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border border-gray-300 rounded-lg appearance-none focus:outline-none focus:ring-2 focus:ring-[#383f56] focus:border-transparent"
+                    id="comment"
+                    value={message}
+                    placeholder={props.placeholderThree}
+                    name="comment"
+                    rows="5"
+                    cols="40"
+                  ></textarea>
+                </label>
+              </div>
+              <div className="col-span-2 text-right">
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    let data = { name, email, message };
+                    fetch("/api/contact", {
+                      method: "POST",
+                      headers: {
+                        Accept: "application/json, text/plain, */*",
+                        "Content-Type": "application/json",
+                      },
+                      body: JSON.stringify(data),
+                    }).then((res) => {
+                      console.log("Response received");
+                      if (res.status === 200) {
+                        console.log("Response succeeded!");
+                        setSubmitted(true);
+                        setName("");
+                        setEmail("");
+                        setMessage("");
+                        setSendText("Sent!");
+                        setButtonActive(false);
+                        setSendButton(
+                          "py-2 px-4 bg-green-400 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg "
+                        );
+                      }
+                    });
+                  }}
+                  type="submit"
+                  disabled={!buttonActive}
+                  className={sendButton}
+                >
+                  {sendText}
+                </button>
+              </div>
             </div>
-            <div className="col-span-2 text-left">
-              {ticked && (
-                <>
-                  <div class="relative pt-1">
-                    <label for="customRange2" class="form-label">
-                      Example range
-                    </label>
-                    <input
-                      type="range"
-                      class="
-      form-range
-      appearance-none
-      w-full
-      h-6
-      p-0
-      bg-transparent
-      focus:outline-none focus:ring-0 focus:shadow-none
-    "
-                      min="0"
-                      max="5"
-                      id="customRange2"
-                    />
-                  </div>
-                </>
-              )}
-            </div> */}
-
-            <div className="col-span-2 text-right">
-              <button
-                type="submit"
-                className="py-2 px-4  bg-[#383f56] hover:bg-gray-600 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
-              >
-                Send
-              </button>
-            </div>
-          </div>
+          )}
         </div>
       </form>
     </>
---
+++ pages/api/contact.js
@@ -0,0 +1,76 @@
+export default function (req, res) {
+  //   let nodemailer = require("nodemailer");
+
+  //   async function main() {
+  //     console.log(req.body);
+
+  //     const transporter = nodemailer.createTransport({
+  //       port: 465,
+  //       host: "smtp.strato.de",
+  //       auth: {
+  //         user: "info@blockchainpresence.net",
+  //         pass: "dslkaddlnvlDKVNFE",
+  //       },
+  //       secure: true,
+  //     });
+
+  //     let info = await transporter.sendMail({
+  //       from: "info@blockchainpresence.net",
+  //       to: "michaelgubler@gmx.net",
+  //       subject: `Message From ${req.body.name}`,
+  //       text: req.body.message,
+  //       html: <div>{req.body.message}</div>,
+  //     });
+  //     console.log("Message sent: %s", info.messageId);
+  //     // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  //     // Preview only available when sending through an Ethereal account
+  //     console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  //     // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  //   }
+
+  //   main().catch(console.error);
+
+  "use strict";
+  const nodemailer = require("nodemailer");
+
+  // async..await is not allowed in global scope, must use a wrapper
+  async function main() {
+    // Generate test SMTP service account from ethereal.email
+    // Only needed if you don't have a real mail account for testing
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true, // true for 465, false for other ports
+      auth: {
+        user: "noreply.blockchainpresence@gmail.com", // generated ethereal user
+        pass: "bcporacle19", // generated ethereal password
+      },
+    });
+
+    // send mail with defined transport object
+    let info = await transporter
+      .sendMail({
+        from: '"Blockchain Presence" <info@blockchainpresence.net>', // sender address
+        to: "michaelgubler@gmx.net", // list of receivers
+        subject: `New Message from ${req.body.name} (${req.body.email})`, // Subject line
+        text: req.body.message, // plain text body
+        html: `<b>${req.body.message}</b>`, // html body
+      })
+      .catch(() => {
+        res.status(500);
+      });
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  }
+
+  main()
+    .then(res.status(200).json({ message: "success" }))
+    .catch(console.error);
+}
