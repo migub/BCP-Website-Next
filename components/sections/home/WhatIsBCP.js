ffcd8c2b5559dd178e64dfd8b4ce5ca1983beb45
donmiguel9 <michaelgubler@gmx.net>
2022-06-10 15:10:07 +0200

    responsive video

--- components/sections/home/WhatIsBCP.js
+++ components/sections/home/WhatIsBCP.js
@@ -8,19 +8,17 @@ const WhatIsBCP = (props) => {
           <h1 className="text-3xl font-bold text-center text-gray-800 sm:text-center sm:text-4xl">
             {props.title}
           </h1>
-          <div className="flex flex-col items-center justify-center mt-20 xl:space-x-32 xl:flex-row sm:px-14 ">
+          <div className="flex flex-col items-center justify-center mt-20 xl:space-x-32 xl:w-[1100px] m-auto xl:flex-row sm:px-14 ">
             <div className="w-full m-auto lg:w-1/2">
               <p className="text-lg text-gray-500 sm:p-4 sm:text-xl">
                 {props.description}
               </p>
             </div>
-            <div className="flex items-center justify-center w-full m-auto mt-10 sm:w-1/2 sm:mt-0">
+            <div className="flex items-center justify-center w-[400px] h-[220px] xl:w-[800px] xl:h-[300px] mt-10 sm:mt-0">
               <Iframe
                 url="https://www.youtube.com/embed/O9Xw5lgZoaM"
-                width="950px"
-                height="450px"
                 id="myId"
-                className="shadow-xl rounded-xl"
+                className="w-full h-full shadow-xl rounded-xl"
                 display="initial"
                 position="relative"
               />
