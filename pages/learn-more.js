4a471a583fc7c2a52cedb5c20b86b73566a90582
hkpz <haticekuebra.parmaksiz@uzh.ch>
2022-06-20 17:49:58 +0200

    reports added

--- pages/learn-more.js
+++ pages/learn-more.js
@@ -65,10 +65,7 @@ const LearnMore = () => {
           </div>
           <div className="flex flex-col items-center justify-center w-1/4 space-y-10 font-bold text-center ">
             <div>
-              Read the report of our strategy team about the top smart contract
-              platforms
-            </div>
-
+              Read the report of our strategy team about the top smart contract platforms.</div>
             <Button
               onClick={() =>
                 download("/static/top_smart_contract_platforms.pdf")
@@ -76,6 +73,72 @@ const LearnMore = () => {
             >
               View Report
             </Button>
+          </div>
+        </div>
+
+        <div className="relative flex w-full h-[300px] space-x-20  justify-center mt-20">
+          <div className="flex flex-col items-center justify-center w-1/4 space-y-10 font-bold text-center ">
+            <div>Read the report of our strategy team about IOTA. </div>
+            <Button
+              onClick={() =>
+                download("/static/IOTA_Research_Report.pdf")
+              }
+            >
+              View Slide Deck
+            </Button>
+          </div>
+          <div className="relative w-1/4 h-full">
+            <Image
+              src="/img/iota.jpg"
+              alt=""
+              layout="fill"
+              objectFit="contain"
+            />
+          </div>
+        </div>
+
+        <div className="relative flex w-full h-[300px] space-x-20  justify-center mt-20 ">
+          <div className="relative w-1/4 h-full">
+            <Image
+              src="/img/bookmarkers.jpg"
+              alt=""
+              layout="fill"
+              objectFit="contain"
+            />
+          </div>
+          <div className="flex flex-col items-center justify-center w-1/4 space-y-10 font-bold text-center ">
+            <div>
+              Read the report of our strategy team about the bookmarkers with oracles.
+            </div>
+
+            <Button
+              onClick={() =>
+                download("/static/Bookmarkers.pdf")
+              }
+            >
+              View Report
+            </Button>
+          </div>
+        </div>
+
+        <div className="relative flex w-full h-[300px] space-x-20   justify-center mt-20">
+          <div className="flex flex-col items-center justify-center w-1/4 space-y-10 font-bold text-center ">
+            <div>Read the report of our strategy team about the Ethereum Layer 2 solution.</div>
+            <Button
+              onClick={() =>
+                download("/static/Ethereum_Layer_2.pdf")
+              }
+            >
+              View Report
+            </Button>
+          </div>
+          <div className="relative w-1/4 h-full">
+            <Image
+              src="/img/ethereum_layer_2.jpg"
+              alt=""
+              layout="fill"
+              objectFit="contain"
+            />
           </div>
         </div>
         <div className="relative flex flex-col items-center justify-center w-full mt-32 lg:flex-row lg:space-x-20 ">
