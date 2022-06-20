c4072d831878c510eda516a92cc5879a3aabc372
donmiguel9 <michaelgubler@gmx.net>
2022-06-20 13:37:10 +0200

    added video to internships

--- pages/internships.js
+++ pages/internships.js
@@ -6,6 +6,7 @@ import TitleSection from "../components/TitleSection";
 import Application from "../components/sections/application/Application";
 import StudentExperience from "../components/sections/application/StudentExperience";
 import StudentExperience2 from "../components/sections/application/StudentExperience2";
+import Iframe from "react-iframe";
 const StudentIntership = () => {
   const router = useRouter();
   return (
@@ -16,13 +17,18 @@ const StudentIntership = () => {
         buttonExternTitle="Start Application"
         buttonExternLink="https://forms.office.com/Pages/ResponsePage.aspx?id=2zjkx2LkIkypCsNYsWmAs7Ovbd1yb0BMjRrifH1U99BURDgyRU0xTExKWjI2MU1NUEVOQ084T0g5Ny4u"
       />
-      {/* <Application
-        title="Application Process"
-        subTitle="University students have the opportunity to write their masters thesis, bachelors thesis, or Individual Lerning Unit at BCP. Interested students first have to attend one of the Info Sessions that are held regularly on Zoom. These Info Session provide the students with  informations about BCP and the opportunity to write their thesis with us. 
-        The participants of the Info Session are provided a link to submit their application. After reviewing the applications, the HR team of BCP conducts interviews with the students. The qualified students receive an offer to work with BCP and in case of an acceptance, they are ready to start working with us."
-        buttonTitle="Start Application"
-        buttonLink="https://forms.office.com/Pages/ResponsePage.aspx?id=2zjkx2LkIkypCsNYsWmAs7Ovbd1yb0BMjRrifH1U99BURDgyRU0xTExKWjI2MU1NUEVOQ084T0g5Ny4u"
-      /> */}
+      <div className="flex items-center justify-center w-full mt-20">
+        <div className="flex items-center justify-center w-[400px] h-[220px] lg:w-[800px] lg:h-[400px] mt-10 sm:mt-0">
+          <Iframe
+            url="https://www.youtube.com/embed/l6zENWETpNA"
+            id="myId"
+            className="w-full h-full shadow-xl rounded-xl"
+            display="initial"
+            position="relative"
+          />
+        </div>
+      </div>
+
       <StudentExperience
         quote1={`“Developing a product at the frontline of blockchain technology, Blockchain Presence challenged me to go beyond 
         the scope of my studies to work on solutions for its disruptive oracle model.”
